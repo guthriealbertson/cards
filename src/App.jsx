f8f28e9d96c75b4c124087fff263d66e2eae775f
guthriealbertson <tui50484@temple.edu>
2023-04-08 18:58:45 -0400

    Color changes and format

--- src/App.jsx
+++ src/App.jsx
@@ -11,9 +11,11 @@ function App() {
     <div className="App">
       <p>{data[choice].Category}</p>
       <h1>{data[choice].Question}</h1>
-      <button onClick={() => setChoice(Math.floor(Math.random() * 150))}>
-        next
-      </button>
+      <div className="next">
+        <button onClick={() => setChoice(Math.floor(Math.random() * 150))}>
+          next
+        </button>
+      </div>
     </div>
   );
 }
